f7ee522414bed8e273393ac822ad760851f46b30
Harm Tilburgs <htilburgs@live.com>
2019-04-02 20:38:13 +0200

    Update MMM-MyPrayerTimes.js

--- MMM-MyPrayerTimes.js
+++ MMM-MyPrayerTimes.js
@@ -43,16 +43,16 @@ Module.register('MMM-MyPrayerTimes', {
 		return false;
 	},
 	
-	convertTime: function (time) {
-  	// Check correct time format and split into components
-  	time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
+	convert24Time: function timeConvert (time) {
+		// Check correct time format and split into components
+		time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
 
-  	if (time.length > 1) { // If time format correct
-    	time = time.slice (1);  // Remove full string match value
-    	time[5] = +time[0] < 12 ? 'AM' : 'PM'; // Set AM/PM
-    	time[0] = +time[0] % 12 || 12; // Adjust hours
-  		}
-  	return time.join (''); // return adjusted time or original string
+		if (time.length > 1) { // If time format correct
+		time = time.slice (1);  // Remove full string match value
+		time[5] = +time[0] < 12 ? 'AM' : 'PM'; // Set AM/PM
+		time[0] = +time[0] % 12 || 12; // Adjust hours
+			}
+		return time.join (''); // return adjusted time or original string
 	},
 	
 	start: function() {
@@ -98,7 +98,7 @@ Module.register('MMM-MyPrayerTimes', {
 		
 		var FajrTimeCell = document.createElement("td");
 		FajrTimeCell.className = "fajr-time bright";
-		FajrTimeCell.innerHTML = convertTime(MPT.Fajr);
+		FajrTimeCell.innerHTML = convert24Time (MPT.Fajr);
 //		FajrTimeCell.innerHTML = (this.config.show24Clock != false ? moment(MPT.Fajr, ["HH:mm"]).format("h:mm A") : MPT.Fajr);		
 
 		FajrRow.appendChild(FajrTimeCell);
