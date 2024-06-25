0cdc23a25ea1d7654a69c9ba52ec14d9e0c67aa2
sam detweiler <sdetweil@gmail.com>
2024-06-25 10:11:42 -0500

    update formatting and fix date in url

--- node_helper.js
+++ node_helper.js
@@ -18,30 +18,35 @@ module.exports = NodeHelper.create({
           console.log("Starting node_helper for: " + this.name);
   },
 
+  //  don't need this anymore with async below
   getMPT: function(url) {
         // Make a GET request using the Fetch API
         fetch(url)
-        .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok');/// can't throw, no one to catch it
+              // also, user doesn't get any feedback on error.. so screen looks stuck..
+              // should send a different notification and put up an error message
+            }
+            return response.json();
+          })
 
-    .then(result => {
-    // Process the retrieved user data
-    // console.log(result.data.timings); // Remove trailing slashes to display data in Console for testing
-    this.sendSocketNotification('MPT_RESULT', result.data.timings);
-    })
+          .then(result => {
+            // Process the retrieved user data
+            // console.log(result.data.timings); // Remove trailing slashes to display data in Console for testing
+            this.sendSocketNotification('MPT_RESULT', result.data.timings);
+          })
 
-    .catch(error => {
-      console.error('Error:', error);
-     });
+          .catch(error => {
+            console.error('Error:', error);
+          });
   },
 
-  socketNotificationReceived: function(notification, payload) {
+ socketNotificationReceived: function(notification, payload) {
             if (notification === 'GET_MPT') {
             this.getMPT(payload);
             }
-  }
+  },
+
+
 });
